521cc9f6b361b04c1a17ab4164f60526cd5d0daa
adammcc <ajm845@gmail.com>
2013-11-08 15:39:50 -0500

    Adams first commit

--- spec/controllers/stocks_controller_spec.rb
+++ spec/controllers/stocks_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe StocksController do
+	before(:all) do
+		Stock.create([{price:{},information:{}},{price:{},information:{}}]) 
+	end
+
 	describe "GET #index" do
+
 		it "responds successfully with HTTP 200 status code" do
 			get :index
 			expect(response).to be_successful
-			expect(response.status).to eq(200) 
+			expect(response.status).to eq(200)
 		end
+
+		it "instantiates @stocks array" do
+			@stocks.class == Array
+		end
+
 	end
 end
