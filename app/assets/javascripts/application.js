7aaa965776d1dcd5595d16fa2bae2e399a010777
skud <st4sik@gmail.com>
2013-11-12 22:41:38 -0500

    comment backbone collections asset

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //= require backbone
 //= require app
 //= require_tree ./models
-//= require_tree ./collections
+// require_tree ./collections
 //= require_tree ./views
 //= require_tree ./routers
 //= require_tree .
