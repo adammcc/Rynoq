f8b8a6b87548afaf6d39275db53c5c493c000035
skud <st4sik@gmail.com>
2013-11-14 10:16:08 -0500

    debugging

--- app/controllers/stock_controller.rb
+++
@@ -1,9 +0,0 @@
-class StockController < ApplicationController
-
-	def battle
-		@select_options_list = Stock.pluck(:company_name, :ticker).collect do |stock| 
-			["#{stock[0]} (#{stock[1]})", stock[1]]
-		end
-	end
-
-end
